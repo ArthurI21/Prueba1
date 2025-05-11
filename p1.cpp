a30db08187e82022abf51a6bc46a1594aad997e0
ArthurI21 <arturoisra21@gmail.com>
2025-05-11 03:14:22 -0600

    Create p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 600
+
+int main(){
+    int tam1;
+    char carrera1[30]-
+    char carrera2[40]="La Banana";
+
+    puts("Dame una carrera: ");
+    gets(carrera1);
+
+    printf("\nOpciones para imprimir una cadena:\n");
+    printf("\n La carrera que capturaste es %s",carrera1);
+
+    printf("\n"):
+    system("pause");
+    return 0;
+}
